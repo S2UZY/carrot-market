02c2d9b4ae225c43a7e263a1105bc860df449351
S2UZY <pc_suzi@daum.net>
2024-12-02 01:35:50 +0900

    feat: add home page with welcome message and navigation links

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
+import { FireIcon } from "@heroicons/react/16/solid";
+import Link from "next/link";
+
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <div className="flex flex-col items-center justify-between min-h-screen p-6">
+      <div className="my-auto flex flex-col items-center gap-3 *:font-medium">
+        <FireIcon className="size-24 w-full text-red-400" />
+        <h1 className="text-4xl pt-2">불꽃 마켓</h1>
+        <h2 className="text-2xl">불꽃 마켓에 어서오세요!</h2>
+      </div>
+      <div className="flex flex-col items-center gap-3 w-full">
+        <Link href="/create-account" className="primary-btn text-lg py-2.5">
+          시작하기
+        </Link>
+        <div className="flex gap-2">
+          <span>이미 계정이 있나요?</span>
+          <Link href="/log-in" className="hover:underline">
+            로그인
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
