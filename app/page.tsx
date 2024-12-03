0b45421859dc19d57a0e4c050654a999163f16e4
S2UZY <pc_suzi@daum.net>
2024-12-03 23:01:06 +0900

    feat: add tweets page - Implement tweet list view - Add getMoreTweets server action - Style tweets page with Tailwind CSS

---
+++ app/page.tsx
@@ -0,0 +1,33 @@
+import TweetList from "@/components/tweet-list";
+import db from "@/lib/db";
+import { Prisma } from "@prisma/client";
+
+async function getInitialTweets() {
+  const tweets = await db.tweet.findMany({
+    select: {
+      id: true,
+      create_at: true,
+      update_at: true,
+      Like: true,
+      user: true,
+      tweet: true,
+    },
+    take: 6,
+    orderBy: {
+      create_at: "desc",
+    },
+  });
+
+  return tweets;
+}
+
+export type InitialTweets = Prisma.PromiseReturnType<typeof getInitialTweets>;
+
+export default async function Tweets() {
+  const initialTweets = await getInitialTweets();
+  return (
+    <div>
+      <TweetList initialTweets={initialTweets} />
+    </div>
+  );
+}
